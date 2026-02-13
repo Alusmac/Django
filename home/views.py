992aa420e8fd13ca3f59839e21f7154e4276a316
Alus <avp3085@gmail.com>
2026-02-13 14:24:11 +0100

    commit

--- home/views.py
+++ home/views.py
@@ -1,18 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
-def home_view(request):
+
+def home_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse("****** Wellcome to My Home Page! ******")
 
-def about_view(request):
+
+def about_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse("****** About page ******")
 
-def contact_view(request):
+
+def contact_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse("****** Contact page ******")
 
-def post_view(request, id):
+
+def post_view(request: HttpRequest, id: int) -> HttpResponse:
     return HttpResponse(f"You are viewing a post from {id}")
-def profile_view(request, username):
+
+
+def profile_view(request: HttpRequest, username: str) -> HttpResponse:
     return HttpResponse(f"You are viewing the user profile: {username}")
 
-def event_view(request, year, month, day):
+
+def event_view(request: HttpRequest, year: int, month: int, day: int) -> HttpResponse:
     return HttpResponse(f"Date of view: {year}-{month}-{day}")
