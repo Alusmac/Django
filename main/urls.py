774493d25a79aebc4fdae91e21e84dcac839983e
Alus <avp3085@gmail.com>
2026-02-16 14:37:23 +0100

    commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from .views import ContactView, ServiceView
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('service/', ServiceView.as_view(), name='service'),
+]
