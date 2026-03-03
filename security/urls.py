353d1f3cdc8ca9daaaf5bd9ecca6811d0e2c9d79
Alus <avp3085@gmail.com>
2026-03-03 13:11:15 +0100

    commit

--- security/urls.py
+++ security/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path("sql-example/", views.safe_sql_example, name="sql_example"),
 ]
--- security/views.py
+++ security/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from .forms import RegisterForm
+from django.db import connection
+from django.contrib.auth import get_user_model
 
 
 def register_view(request: HttpRequest) -> HttpResponse:
@@ -58,3 +60,30 @@ def logout_view(request: HttpRequest) -> HttpResponse:
     """
     logout(request)
     return redirect("/login/")
+
+
+def safe_sql_example(request: HttpRequest) -> HttpResponse:
+    """safe SQL execution using parameterized queries.
+    """
+
+    username = request.GET.get("username", "")
+    result = []
+
+    User = get_user_model()
+    table_name = User._meta.db_table
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f"SELECT username FROM {table_name} WHERE username = %s",
+            [username],
+        )
+        result = cursor.fetchall()
+
+    return render(
+        request,
+        "security/sql_example.html",
+        {
+            "username": username,
+            "result": result,
+        },
+    )
